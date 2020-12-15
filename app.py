4eab02dde144777629d32b1e410dc8fbfe5583c4
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-12-15 15:50:58 +0700

    Edit forms

--- app.py
+++ app.py
@@ -118,42 +118,11 @@ def index():
 def venues():
     #  replace with real venues data.
     #       num_shows should be aggregated based on number of upcoming shows per venue.
-    data = []
-    venues = Venue.query.all()
+    data = {"": [v.local for v in db.get_venues_locals()]}
+    for d in data[""]:
+        d["venues"] = [v.serialize for v in db.get_all_venues(
+        ) if v.city == d["city"] and v.state == d["state"]]
 
-    locations = set()
-
-    for venue in venues:
-        # add city/state tuples
-        locations.add((venue.city, venue.state))
-
-    # for each unique city/state, add venues
-    for location in locations:
-        data.append({
-            "city": location[0],
-            "state": location[1],
-            "venues": []
-        })
-
-    for venue in venues:
-        num_upcoming_shows = 0
-
-        shows = Show.query.filter_by(venue_id=venue.id).all()
-
-        # get current date to filter num_upcoming_shows
-        current_date = datetime.now()
-
-        for show in shows:
-            if show.start_time > current_date:
-                num_upcoming_shows += 1
-
-        for venue_location in data:
-            if venue.state == venue_location['state'] and venue.city == venue_location['city']:
-                venue_location['venues'].append({
-                    "id": venue.id,
-                    "name": venue.name,
-                    "num_upcoming_shows": num_upcoming_shows
-                })
         return render_template('pages/venues.html', areas=data)
 
 
@@ -265,6 +234,25 @@ def show_venue(venue_id):
 @app.route('/venues/create', methods=['GET'])
 def create_venue_form():
     form = VenueForm()
+    try:
+        new_venue = Venue(
+            name=form.name.data,
+            city=form.city.data,
+            state=form.state.data,
+            address=form.address.data,
+            phone=form.phone.data,
+            genres=form.genres.data,
+            facebook_link=form.facebook_link.data,
+            image_link=form.image_link.data,
+            website=form.website.data,
+            seeking_talent=form.seeking_talent.data,
+            seeking_description=form.seeking_description.data
+        )
+        db.create_venue(new_venue)
+        flash('Venue ' + request.form['name'] + ' was successfully listed!')
+    except ValueError:  # FIXME melhorar essa exception
+        flash(
+            'An error occurred. Venue ' + form.name + ' could not be listed.')
     return render_template('forms/new_venue.html', form=form)
 
 
